e7d376c2bff7f376edb1fe6b9fac508e2973cbe9
Patrick <seitzpr@gmail.com>
2023-09-05 13:54:28 -0400

    Practice flexbox

--- backend/server.py
+++ backend/server.py
@@ -1,26 +1,36 @@
 # Import flask and datetime module for showing date and time
-from flask import Flask
+from flask import Flask, send_file
 import datetime
- 
+import os
+
+
 x = datetime.datetime.now()
- 
+
 # Initializing flask app
 app = Flask(__name__)
- 
- 
+
+
 # Route for seeing a data
-@app.route('/data')
+@app.route("/data")
 def get_time():
- 
     # Returning an api for showing in  reactjs
-    return {
-        'Name':"geek",
-        "Age":"22",
-        "Date":x,
-        "programming":"python"
-        }
- 
-     
+    return {"Name": "John Doe", "Age": "10000", "Date": x, "programming": "python"}
+
+
+def sendImage():
+    image_path = "images/research_combined_histogram_2023-08-10.png"
+    # img_dir = "backend/images"
+    # img_list = os.listdir(img_dir)
+    # img_path = os.path.join(img_dir, random.choice(img_list))
+    return image_path
+
+
+@app.route("/image")
+def myapp():
+    image = sendImage()
+    return send_file(image, mimetype="image/png")
+
+
 # Running app
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- front/src/routes/user.js
+++ front/src/routes/user.js
@@ -1,11 +1,63 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
+import { Container, TextField } from "@mui/material";
 
 function User() {
+
+  const [data, setdata] = useState({
+    name: "",
+    age:0,
+    date: "",
+    programming: "",
+  });
+
+  useEffect(() => {
+    fetch("/data").then((res) =>
+      res.json().then((data) => {
+        setdata({
+          name: data.Name,
+          age: data.Age,
+          date: data.Date,
+          programming: data.programming,
+        });
+      })
+    );
+  }, []);
+  
   return (
-    <div className="home">
-      <h1>User</h1>
+    <div>
+      <Container fixed>
+        <TextField id="outlined-basic" variant="filled" fullWidth="25" label='Search' />
+      </Container> 
+      <div style={{textAlign: "center", height: "100%" }}>
+        {/* <p>{data.name}</p>
+        <p>{data.age}</p>
+        <p>{data.date}</p>
+        <p>{data.programming}</p>
+        <div style={{ background: "red" }}>content</div> */}
+        <Container fixed>
+        <img style={{ width: 600, height: 400 }} src={"http://localhost:5000/image"} alt=""/>
+        </Container>
+      </div> 
+
+      <Container>
+        <div className='container'>
+
+          <div className="column">index
+            <div className="info">info</div>
+          </div>
+
+          <div className="column" style={{backgroundColor: "pink", alignSelf: 'stretch'}}>
+            <div className="search-bar">search
+              <div className="info" style={{backgroundColor: "purple"}}>info</div>
+            </div>
+          </div>
+          
+        </div>
+      </Container>
+  
     </div>
   );
+
 }
 
 export default User;
